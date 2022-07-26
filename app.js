35b2db3a0701f367ce4d263f03c8da1b26e5647f
unknown <ogunsuyijoseph360@gmail.com>
2022-07-26 16:59:58 +0100

    initial state

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+// import myJson from './example.json' assert {type: 'json'};
+const countryNumber = require('./countryNum.json');
+app.get('/', (req, res) => {
+  res.json (countryNumber);
+})
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`)
+});
